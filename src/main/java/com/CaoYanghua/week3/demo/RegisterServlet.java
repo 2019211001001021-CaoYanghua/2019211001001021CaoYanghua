4e987440b8890e2478851548068341ad71a8f724
2019211001001021CaoYanghua <1354437507@qq.com>
2021-03-31 14:02:27 +0800

    HW-week4 Commit

--- src/main/java/com/CaoYanghua/week3/demo/RegisterServlet.java
+++ src/main/java/com/CaoYanghua/week3/demo/RegisterServlet.java
@@ -1,14 +1,44 @@
 package com.CaoYanghua.week3.demo;
 
+import com.CaoYanghua.week4.demo.User;
+
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 //automatic -new-->servlet
+@WebServlet(
+        urlPatterns = {"/register"}
+)
 public class RegisterServlet extends HttpServlet {
+
+    Connection con = null;
+    @Override
+    public void init() throws ServletException {
+        /* week 4 code */
+        ServletContext servletContext = getServletContext();
+        String driver =  servletContext.getInitParameter("driver");
+        String url =  servletContext.getInitParameter("url");
+        String username =  servletContext.getInitParameter("username");
+        String password =  servletContext.getInitParameter("password");
+        System.out.println(driver+url+username+password);
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,username,password);
+            System.out.println("init()-->"+con);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 
     }
 
@@ -16,7 +46,7 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //request come here- <from method=post>
         //get parameter from request
-        String username = request.getParameter( "username");//name of input type<input type="text" name="username">
+       /* String username = request.getParameter( "username");//name of input type<input type="text" name="username">
         String password = request.getParameter( "password");//<input type="password" name="password"/>
         String email = request.getParameter( "email");//<input type="text" name="email"/>
         String gender = request.getParameter( "gender");//<input type="radio" name="gender">
@@ -29,6 +59,95 @@ public class RegisterServlet extends HttpServlet {
         writer.println("<br>email :"+username);
         writer.println("<br>gender :"+username);
         writer.println("<br>birth Date :"+username);
-        writer.close();
+        writer.close(); */
+
+        List<User> list = new ArrayList<>();
+        String username =  request.getParameter("username");
+        String password =  request.getParameter("password");
+        String email =  request.getParameter("email");
+        String gender =  request.getParameter("gender");
+        String birthDate =  request.getParameter("birthDate");
+
+        String sql = "INSERT INTO usertable(username,password,email,gender,birthdate) VALUES (?,?,?,?,?)";
+
+        int resultSet;
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1,username);
+            ps.setString(2,password);
+            ps.setString(3,email);
+            ps.setString(4,gender);
+            ps.setString(5,birthDate);
+            resultSet = ps.executeUpdate();
+            ps.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        String sql2 = "SELECT * FROM usertable";
+        Statement statement = null ;
+        ResultSet resultSet2 = null ;
+
+        try {
+            statement = con.createStatement();
+            resultSet2 = statement.executeQuery(sql2);
+            while (resultSet2.next())
+            {
+                User user = new User();
+                user.setId(resultSet2.getInt(1));
+                user.setUsername(resultSet2.getString(2));
+                user.setPassword(resultSet2.getString(3));
+                user.setEmail(resultSet2.getString(4));
+                user.setGender(resultSet2.getString(5));
+                user.setBirthDate(resultSet2.getString(6));
+                list.add(user);
+            }
+            resultSet2.close();
+            statement.close();
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            PrintWriter writer = null;
+            try {
+                writer = response.getWriter();
+                response.setContentType("text/html;charset=utf-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            writer.println("<table border=\"1\">");
+            writer.println("<tr>");
+            writer.println("<td>ID</td>");
+            writer.println("<td>UserName</td>");
+            writer.println("<td>Password</td>");
+            writer.println("<td>Email</td>");
+            writer.println("<td>Gender</td>");
+            writer.println("<td>Birthday</td>");
+            writer.println("</tr>");
+            for (User user : list) {
+                writer.println("<tr>");
+                writer.println("<td>"+user.getId()+"</td>");
+                writer.println("<td>"+user.getUsername()+"</td>");
+                writer.println("<td>"+user.getPassword()+"</td>");
+                writer.println("<td>"+user.getEmail()+"</td>");
+                writer.println("<td>"+user.getGender()+"</td>");
+                writer.println("<td>"+user.getBirthDate()+"</td>");
+                writer.println("</tr>");
+            }
+            writer.println("</table>");
+            writer.close();
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/CaoYanghua/week4/demo/User.java
@@ -0,0 +1,57 @@
+package com.CaoYanghua.week4.demo;
+public class User {
+    public String username;
+    public String password ;
+    public String email ;
+    public String gender ;
+    public String birthDate ;
+    public int Id;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+}
